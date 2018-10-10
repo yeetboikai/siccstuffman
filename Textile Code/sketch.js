921dc2d1ee9840e0648c99496767d905b1884002
yeetboikai <43797546+yeetboikai@users.noreply.github.com>
2018-10-10 09:09:02 -0700

    Update sketch.js

--- Textile Code/sketch.js
+++ Textile Code/sketch.js
@@ -6,26 +6,19 @@ let y;
 
 function preload() {
   img1 = loadImage('assets/spoopy_pig.png');
-  img2 = loadImage('assets/wowzers_alligator.png');
-  img3 = loadImage('assets/sharky_shark.jpg');
 
 }
 function setup() {
 createCanvas (1920,1080);
 background (255,0,0);
 imageMode (CENTER);
+
 }
 
 function draw() {
-x=random (0,1920);
-y=random(0,1080);
-image(img1, x, y);
-image(img2, x+200, y+200);
-image(img3, x+300, y+300);
-}
+  scale(1/5);
+  image(img1, random (1920,0), random(0,1080));
+  image(img2, random (1920,0), random(0,1080));
+  image(img3, random (1920,0), random(0,1080));
 
-for (var i = 0; i < 100; i++) {
-  var r = random(50);
-  stroke(r * 5);
-  line(50, i, 50 + r, i);
 }
